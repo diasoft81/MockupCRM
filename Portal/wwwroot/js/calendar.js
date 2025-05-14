9396813e1bfec2d06e3431e7e03c40b9ef47fc12
diasoft81 <diasoft.web.id@gmail.com>
2025-05-14 10:33:36 +0700

    Add Calendar Activity

---
+++ Portal/wwwroot/js/calendar.js
@@ -0,0 +1,18 @@
+﻿window.calendarInterop = {
+    renderCalendar: function (elementId, eventsJson) {
+        const calendarEl = document.getElementById(elementId);
+        const events = JSON.parse(eventsJson);
+
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,listWeek'
+            },
+            events: events
+        });
+
+        calendar.render();
+    }
+};
